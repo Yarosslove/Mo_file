1b8dfe21f11c41286ee68106f5baa97e13b6e73d
Jaroslav Prejza <yarosslove@seznam.cz>
2022-10-11 11:38:56 +0200

    Initial commit

--- JustStartedProj/Mo_file.py
+++ JustStartedProj/Mo_file.py
@@ -1,1 +1,8 @@
+<<<<<<< HEAD
 print("crazy")
+=======
+
+print("always205")
+
+print("boom boom")
+>>>>>>> a65f98f (Initial commit)
---
+++ JustStartedProj/StartingFile.py
@@ -0,0 +1,3 @@
+print("how are you")
+print("how are you")
+print("how are you")
